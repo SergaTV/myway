91e56b9d91d3260e5c8752dcfc797ba6402e6a97
Sergey Sit'notyourdeal <63511782+SergaTV@users.noreply.github.com>
2022-03-07 10:21:12 +0300

    FIXED: sequence fibbonaci.c

--- fibbonaci.c
+++ fibbonaci.c
@@ -4,7 +4,8 @@
 
 int main() {
     unsigned long long a=1, b=1;
-    for (int i = 0; i < AMOUNT; ++i) {
+    printf("0");
+    for (int i = 0; i < AMOUNT-1; ++i) {
         printf("%d ", a);
         unsigned long long c = a;
         a = b;
